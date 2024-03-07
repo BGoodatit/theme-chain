3df586dfd1f4d7086939cb1b42517420a8969c9c
BGoodatit <81009488+BGoodatit@users.noreply.github.com>
2024-03-06 21:33:59 -0500

    Add symbols configuration for theme

---
+++ symbols.fish
@@ -0,0 +1,54 @@
+set -gx symbols (aws, battery, character, cmake, cmd_duration, conda, crystal, dart, directory, docker_context, dotnet, elixir, elm, env_var, erlang, gcloud, git_branch, git_commit, git_state, git_status, golang, helm, hostname, java, jobs, julia, kubernetes, line_break, memory_usage, hg_branch, nim, nix_shell, nodejs, ocaml, package, perl, php, purescript, python, ruby, rust, shlvl, singularity, swift, status, terraform, time, username, zig)
+set -gx aws_symbol " "
+set -gx battery_full_symbol ""
+set -gx battery_charging_symbol ""
+set -gx battery_discharging_symbol ""
+set -gx character_success_symbol "[❯](bold green)"
+set -gx character_error_symbol "[❯](bold red)"
+set -gx character_vicmd_symbol "[❮](bold green)"
+set -gx cmake_symbol "喝 "
+set -gx cmd_duration_min_time 2000
+set -gx conda_symbol " "
+set -gx crystal_symbol " "
+set -gx dart_symbol " "
+set -gx directory_truncation_length 3
+set -gx docker_context_symbol " "
+set -gx dotnet_symbol "•NET "
+set -gx elixir_symbol " "
+set -gx elm_symbol " "
+set -gx erlang_symbol " "
+set -gx gcloud_symbol " "
+set -gx git_branch_symbol " "
+set -gx git_commit_commit_hash_length 7
+set -gx git_state_rebase "REBASING"
+set -gx git_status_style "red bold"
+set -gx golang_symbol " "
+set -gx helm_symbol "⎈ "
+set -gx hostname_ssh_only true
+set -gx java_symbol " "
+set -gx jobs_threshold 1
+set -gx julia_symbol " "
+set -gx kubernetes_disabled true
+set -gx line_break_enabled true
+set -gx memory_usage_disabled true
+set -gx hg_branch_symbol " "
+set -gx nim_symbol " "
+set -gx nix_shell_symbol "  "
+set -gx nodejs_symbol " "
+set -gx ocaml_symbol "🐫 "
+set -gx package_symbol " "
+set -gx perl_symbol " "
+set -gx php_symbol " "
+set -gx purescript_symbol "<=> "
+set -gx python_symbol " "
+set -gx ruby_symbol " "
+set -gx rust_symbol " "
+set -gx shlvl_disabled true
+set -gx singularity_symbol " "
+set -gx swift_symbol "ﯣ "
+set -gx status_disabled true
+set -gx terraform_symbol "ﰉ "
+set -gx time_disabled true
+set -gx username_format "[$user]($style) in "
+set -gx zig_symbol "↯ "
+#EOF.
