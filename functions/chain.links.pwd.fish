470d02a7a9d9748d2fb6699ddcf9023d3bb96059
BGoodatit <81009488+BGoodatit@users.noreply.github.com>
2024-03-08 00:53:49 -0500

    Update chain.links.pwd to use full path names and custom prefix

--- functions/chain.links.pwd.fish
+++ functions/chain.links.pwd.fish
@@ -1,22 +1,14 @@
 function chain.links.pwd
-  set -q chain_project_glyph
-    or set -l chain_project_glyph '@'
+  # Use the specific symbol as the prefix instead of @username
+  set chain_project_glyph '><> ~ '
 
-  set -l prefix ''
   set -l path "$PWD"
 
-  # Replace either HOME with ~ or abbreviate project root.
-  if vcs.present
-    functions -q vcs.root || vcs.refresh
-    set path (string replace (vcs.root) '' "$path")
-    set prefix @(basename (vcs.root))
-  else
-    set path (string replace ~ '~' "$path")
-  end
-
-  # Shorten path segments.
-  set path (string replace -ar '(\.?[^/]{1})[^/]*/' '$1/' "$path")
+  # Replace HOME with ~ without abbreviating other parts of the path.
+  set path (string replace -r $HOME '~' "$path")
 
   echo cyan
+  echo "$chain_project_glyph$path"
+end
   echo "$prefix$path"
 end
